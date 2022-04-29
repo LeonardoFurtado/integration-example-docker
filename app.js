a18346e09a51e03430f3b86f0f2037290a6cd02f
Scott Motte <mot@mot.la>
2022-04-29 15:38:14 -0700

    Display hello world

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-var http = require('http')
+require('dotenv').config()
 
+var http = require('http')
 var port = 3001
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'})
-  res.write(`<h1>Hello World!<h1>`)
+  res.write(`<h1>Hello ${process.env.HELLO}!<h1>`)
   res.end() //end the response
 }).listen(port, function(){
   console.log(`server start at port ${port}`)
